21a7491500ea552ee948b609ec09a10d2e5cf2cb
Javis82 <forojavi@gmail.com>
2025-04-05 11:38:25 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,7 @@
         <el-input v-model="loginForm.username"></el-input>
       </el-form-item>
       <el-form-item label="Password">
-        <el-input type="password" v-model="loginForm.password"></el-input>
-      </el-form-item>
-      <el-form-item>
+      
         <el-button type="primary" @click="login()">Login</el-button>
       </el-form-item>
     </el-form>
